0f842a97ecb013a369dd5095de0ee79cebebdb7a
hanheel <hanhee630630@gmail.com>
2025-06-18 17:29:15 +0900

    feat: toggleSwith 컴포넌트 제작

---
+++ src/components/@common/toggleSwitch/ToggleSwitch.tsx
@@ -0,0 +1,36 @@
+import * as S from "./ToggleSwitch.styles";
+import { useState } from "react";
+
+interface ToggleSwitchProps {
+  tabContent: {
+    label: string;
+    onClick: () => void;
+  }[];
+}
+
+const ToggleSwitch = ({ tabContent }: ToggleSwitchProps) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const selectTab = (index: number) => {
+    setSelectedIndex(index);
+    tabContent[index].onClick();
+  };
+
+  return (
+    <S.ToggleSwitchContainer>
+      {tabContent.map((tab, index) => (
+        <S.ToggleSwitchButton
+          key={tab.label}
+          isActive={selectedIndex === index}
+          onClick={() => selectTab(index)}
+        >
+          {tab.label}
+          {selectedIndex === index && (
+            <S.ToggleSwitchUnderline layoutId="underline" />
+          )}
+        </S.ToggleSwitchButton>
+      ))}
+    </S.ToggleSwitchContainer>
+  );
+};
+
+export default ToggleSwitch;
