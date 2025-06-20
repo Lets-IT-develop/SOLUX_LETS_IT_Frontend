10ec75fe7b6a1afc1bcbde66edd2a299946f56a5
hanheel <hanhee630630@gmail.com>
2025-06-21 00:04:35 +0900

    fix: 경로 오류 해결

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
-import { ThemeProvider } from "@emotion/react";
-import { theme } from "./styles/theme";
-import GlobalStyle from "./styles/GlobalStyles";
-import ToggleSwitch from "./components/@common/toggleSwitch/ToggleSwitch";
+import { ThemeProvider } from '@emotion/react';
+import { theme } from './styles/theme';
+import GlobalStyle from './styles/GlobalStyles';
+import { ToggleSwitch } from '../shared';
 
 function App() {
   const tabContent = [
-    { label: "테스트", onClick: () => {} },
-    { label: "테스트2", onClick: () => {} },
+    { label: '테스트', onClick: () => {} },
+    { label: '테스트2', onClick: () => {} },
   ];
   return (
     <ThemeProvider theme={theme}>
