3e0be8ae87a39902008b6e79bba4a40bd7437909
hanheel <hanhee630630@gmail.com>
2025-06-21 22:58:56 +0900

    feat: 파일 업로드 구현

--- src/features/login/model/useLoginFunnel.ts
+++ src/features/login/model/useLoginFunnel.ts
@@ -1,8 +1,9 @@
 import { useFunnel } from '@use-funnel/react-router-dom';
+import { SIGNUP_STEPS } from '../../../shared/constants/steps';
 
 // TODO : interest 타입 정의
 export const useLoginFunnel = () => {
-  return useFunnel<{
+  const funnel = useFunnel<{
     profileImage: { profileImage?: File };
     nickName: { profileImage?: File; nickName: string };
     age: { profileImage?: File; nickName: string; age: string };
@@ -30,4 +31,28 @@ export const useLoginFunnel = () => {
       },
     },
   });
+
+  const currentStep = funnel.step;
+  const nextStep = SIGNUP_STEPS[SIGNUP_STEPS.indexOf(currentStep) + 1];
+
+  const goBackStep = () => {
+    const currentStepIndex = SIGNUP_STEPS.indexOf(funnel.step);
+    if (currentStepIndex > 0) {
+      funnel.history.push(SIGNUP_STEPS[currentStepIndex - 1], funnel.context);
+    }
+  };
+
+  const proceedToNextStep = () => {
+    if (!nextStep) return;
+    funnel.history.push(nextStep);
+  };
+
+  const storeData = (key: string, data: File | string | string[] | undefined) => {
+    funnel.history.replace(currentStep, {
+      ...funnel.context,
+      [key]: data,
+    });
+  };
+
+  return { funnel, goBackStep, storeData, proceedToNextStep };
 };
--- src/features/login/ui/profileImageUploader/ProfileImageUploader.tsx
+++ src/features/login/ui/profileImageUploader/ProfileImageUploader.tsx
@@ -1,6 +1,7 @@
 import * as S from './ProfileImageUploader.styles';
 import defaultProfileImage from '../../../../shared/assets/images/default_profile.svg';
 import uploadIcon from '../../../../shared/assets/icons/ic_camera.svg';
+import { useRef } from 'react';
 
 interface ProfileImageUploaderProps {
   file: File | undefined;
@@ -10,10 +11,29 @@ interface ProfileImageUploaderProps {
 const ProfileImageUploader = ({ file, onFileChange }: ProfileImageUploaderProps) => {
   const previewUrl = file ? URL.createObjectURL(file) : defaultProfileImage;
 
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+    onFileChange(file);
+  };
+
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
   return (
     <S.ProfileImageUploaderContainer>
       <S.ProfileImageUploaderImage src={previewUrl} alt="프로필 이미지 삽입" />
-      <S.ProfileImageUploaderButton>
+      <input
+        ref={fileInputRef}
+        type="file"
+        accept="image/*"
+        style={{ display: 'none' }}
+        onChange={handleFileChange}
+      />
+
+      <S.ProfileImageUploaderButton onClick={handleButtonClick}>
         <img src={uploadIcon} alt="프로필 이미지 삽입" />
       </S.ProfileImageUploaderButton>
     </S.ProfileImageUploaderContainer>
