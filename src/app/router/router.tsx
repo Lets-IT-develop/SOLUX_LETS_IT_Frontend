dd15592c6472d046f7e1cda173fce4974db8dff5
hanheel <hanhee630630@gmail.com>
2025-06-21 22:17:03 +0900

    feat: 라우터 설정

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,5 +1,6 @@
-import { Layout } from '../../pages/loginPage/ui';
+import { Layout } from '../../pages/ui';
 import { createBrowserRouter } from 'react-router-dom';
+import { LoginPage } from '../../pages';
 
 export const router = createBrowserRouter([
   {
@@ -8,7 +9,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        // element: <LoginPage />,
+        element: <LoginPage />,
       },
     ],
   },
