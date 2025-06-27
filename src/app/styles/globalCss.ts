ce5482035a57415d85be595392e9cc619dec1090
hanheel <hanhee630630@gmail.com>
2025-06-27 17:25:42 +0900

    fix: fallback 웹 폰트 설정

--- src/app/styles/globalCss.ts
+++ src/app/styles/globalCss.ts
@@ -2,9 +2,19 @@ import { css } from '@emotion/react';
 import type { Theme } from '@emotion/react';
 
 export const globalCss = (theme: Theme) => css`
+ @font-face {
+   font-family: 'SUIT';
+   src: url('/fonts/SUIT-Variable.woff2') format('woff2');
+   font-display: swap;
+ }
+ body{ 
+  font-family: 'SUIT', 'Noto Sans KR', 'Apple SD Gothic Neo', sans-serif;
+ }
   input {
     &:focus {
       outline: 1px solid ${theme.colors.primary};
     }
   }
+
+
 `;
